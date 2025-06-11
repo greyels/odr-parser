8e2e035b543a2f5493dcf7f8d2103e93a7a8a029
Sergey Grishin <s.grishin@accenture.com>
2025-06-11 13:09:11 +0200

    config template added

---
+++ config/template-config.py
@@ -0,0 +1,10 @@
+EXCEL_FILE_NAME = ""
+EXCEL_FILE_SHEETNAME = ""
+JSON_FILENAME = ""
+
+AL = ""
+KEYWORD_LIST = []
+# To omit level restrictions use "any"
+LEVEL = 8
+# To omit language requirements use ""
+NO_LANGUAGE = ""
